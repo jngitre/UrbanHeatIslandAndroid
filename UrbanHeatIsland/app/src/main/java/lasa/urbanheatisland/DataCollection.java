29c7b28d06731e6fa3b85c1fe4e7667464a21a33
Shivang Singh <shivang.singh51@stu.austinisd.org>
2017-04-21 15:04:14 -0500

    hey

--- UrbanHeatIsland/app/src/main/java/lasa/urbanheatisland/DataCollection.java
+++ UrbanHeatIsland/app/src/main/java/lasa/urbanheatisland/DataCollection.java
@@ -2,6 +2,9 @@ package lasa.urbanheatisland;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class DataCollection extends AppCompatActivity {
 
@@ -10,4 +13,32 @@ public class DataCollection extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_collection);
     }
+
+    // Data retrieval into the database
+    public void submitClick(View v) {
+
+        EditText temp = (EditText) findViewById(R.id.Temperature);
+
+        if(temp.getText().toString().equals("")){
+
+            Toast.makeText(getApplicationContext(),"Please Enter A Valid Temperature Value",Toast.LENGTH_LONG).show();
+
+        }
+
+
+        double temperatureInput = Double.parseDouble(temp.getText().toString());
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
 }
